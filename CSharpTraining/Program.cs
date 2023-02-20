f03ecd1b7c1d3c06d80c39095355d7cc09aaf13d
Gowtham <gowthamk91@outlook.com>
2023-02-20 09:21:56 +0530

    Add project files.

---
+++ CSharpTraining/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+//--------------------------------------//
+
+//-----Raw string literals ---------------//
+var str = """"<input text="""Test""">"""";
+//var jsonStr = "{\n   \"name\":\"Gowtham\"  \n}";
+
+var jsonStr = """
+    {  
+    "name":"Gowtham" 
+    }
+    """;
+Console.WriteLine(jsonStr); 
+Console.WriteLine(str);
+
+//--------------------------------------//
+
+//-----List pattern matching ---------------//
+
+int[] numbers = {1,2,3};
+Console.WriteLine(numbers is [1, 2, 3]); // True 
+Console.WriteLine(numbers is [1, 2, 4]); // False 
+Console.WriteLine(numbers is [1, 2, 3,4]); // False
+Console.WriteLine(numbers is [0 or 1, <= 2, <= 3]);//True
+Console.WriteLine(numbers is [1, _, _]);//True
+
+var emptyName=Array.Empty<string>();
+var myName = new[] { "Gowtham" };
+
+var text = myName switch
+{
+    [] => "Name was empty",
+    [var fullName] => $"My Name is {fullName}"
+};
+Console.WriteLine(text);
+Console.ReadKey();
+
+
+
+
